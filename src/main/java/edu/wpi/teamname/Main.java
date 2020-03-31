c2f78170dc96b072b24fd0a3851f618101622cbf
matthew7758 <entei567@gmail.com>
2020-03-30 20:27:44 -0400

    Added mpmalone println

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -3,7 +3,7 @@ package edu.wpi.teamname;
 public class Main {
 
   public static void main(String[] args) {
-    system.out.println("Hello world!");
+    System.out.println("mpmalone!");
     App.launch(App.class, args);
   }
 }
