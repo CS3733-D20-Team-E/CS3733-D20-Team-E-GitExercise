99676cf482a8d34e58d5462a513d8cf8352696a3
Ha Nguyen <ha._.nguyen@icloud.com>
2020-03-31 07:38:05 +0700

    Cody

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -3,11 +3,6 @@ package edu.wpi.teamname;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-    System.out.println("Alex was here");
-    System.out.println("Matt Tolbert");
-    System.out.println("Sylvia Lin");
-    App.launch(App.class, args);
-    System.out.println("Claire Sellen");
+    System.out.println("Cody");
   }
 }
